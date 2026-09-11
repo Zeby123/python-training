e963adeaa150bb7e4afb316d78b2859fe2376b8e
Zeby123 <janekaroki04@gmail.com>
2026-09-11 21:20:04 +0300

    beginning

---
+++ intro.py
@@ -0,0 +1,9 @@
+print("Jane Karoki")
+jane_karoki="Jane Karoki"
+print(jane_karoki)
+
+total=1000+500
+print(total)
+
+my_name="jane_karoki"
+print(my_name)
---
+++ variable.py
@@ -0,0 +1,6 @@
+#calculate the area of a rectangle
+length=50
+width=40
+area=length*width
+
+print(area)
